99e5cc5cb30189ee4d7a08e863bca4951b1c15b1
Emmanuel-Arredondo <thorough62@gmail.com>
2023-04-19 12:47:13 -0600

    Base de datos?

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express =require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+
 const app = express();
-const port= process.env.port | 3000;
+
 
 const homeRoutes = require('./sr/routes/home.routes');
 const reservationsRoutes = require('./sr/routes/reservations.routes');
@@ -10,7 +14,16 @@ app.use('/',homeRoutes);
 app.use('/reservations',reservationsRoutes);    
 app.use('/users',usersRoutes);
 
+const main=async ()=>{
+    try{
+    await mongoose.connect(process.env.DB_URL);
+    app.listen(process.env.PORT,()=>console.log('Server listening at port'+process.env.PORT));
+    }
+    catch(err){
+        console.log(err);
+    }
+}
 
 
-app.listen(port,()=>console.log('Server listening at port'+port));
+main();
 
